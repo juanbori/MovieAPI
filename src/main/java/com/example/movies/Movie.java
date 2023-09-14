b69ed23ad570751edfca01d6d6b7958819fd7050
juanbori <jsires94@hotmail.com>
2023-09-14 16:39:34 +0200

    Create review added

--- src/main/java/com/example/movies/Movie.java
+++ src/main/java/com/example/movies/Movie.java
@@ -18,17 +18,23 @@ public class Movie {
     @Id
     private ObjectId id;
     private String imdbId;
-
     private String title;
-
     private String releaseDate;
-
     private String trailerLink;
-
     private String poster;
-    private List<String> genres;
     private List<String> backdrops;
-    @DocumentReference()
-    private List<Review> reviewIds;
+    private List<String> genres;
+    @DocumentReference
+    private List<Review> reviews;
 
+    public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres, List<Review> reviews) {
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genres = genres;
+        this.reviews = reviews;
+    }
 }
